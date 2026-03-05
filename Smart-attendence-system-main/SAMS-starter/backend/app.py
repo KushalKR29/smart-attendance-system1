ba2ceeb764db382730c5fe9e6b3294cfb1ea118b
KushalKR29 <kushalkrmce@gmail.com>
2026-03-05 22:41:37 +0530

    fix frontend path

--- Smart-attendence-system-main/SAMS-starter/backend/app.py
+++ Smart-attendence-system-main/SAMS-starter/backend/app.py
@@ -24,9 +24,13 @@ app = Flask(__name__)
 CORS(app)
 
 import os
+from flask import send_from_directory
 
-BASE_DIR = os.path.dirname(__file__)
-FRONTEND_DIR = os.path.join(BASE_DIR, "..", "frontend")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+FRONTEND_DIR = os.path.abspath(
+    os.path.join(BASE_DIR, "..", "frontend")
+)
 
 @app.route("/")
 def home():
